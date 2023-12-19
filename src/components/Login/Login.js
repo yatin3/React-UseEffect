5cc1b0f21e924a81b8516ce5c926feeba5f8a81e
yatin goyal <yatingoyal31@gmail.com>
2023-12-19 18:24:23 +0530

    added context

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer,useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../store/auth-context";
 
 const emailReducer = (state,action) => {
   if (action.type === 'USER_INPUT') {
@@ -31,6 +32,8 @@ const Login = (props) => {
   const [emailState,dispatchEmail] = useReducer(emailReducer, {value:'', isValid:null});
 
   const [passwordState,dispatchPassword] = useReducer(passwordReducer, {value:'', isValid:null});
+
+  const authCtx = useContext(AuthContext);
 
   const { isvalid: emailIsvalid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -76,7 +79,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
